32d12e4c52f40c693a7d2621423d889923ac810c
tanvirakon <akon.pt33@gmail.com>
2026-03-15 22:45:29 +0600

    updated code

--- lib/quiz.dart
+++ lib/quiz.dart
@@ -2,37 +2,25 @@ import 'package:flutter/material.dart';
 import 'package:quiz_app/question.dart';
 import 'package:quiz_app/start_screen.dart';
 
-class Quiz extends StatefulWidget{
-  State<Quiz> createState(){
+class Quiz extends StatefulWidget {
+  const Quiz({super.key});
+  @override
+  State<Quiz> createState() {
     return _QuizState();
   }
 }
 
-class _QuizState extends State<Quiz>{
+class _QuizState extends State<Quiz> {
+  Widget activeScreen = const StartScreen();
 
-  Widget activeScreen = StartScreen(switchScreen);
-  void switchScreen(){
+  void switchScreen() {
     setState(() {
-      activeScreen = QuestionScreen();
+      activeScreen = const QuestionScreen();
     });
   }
 
   @override
-  Widget build(context){
-    return MaterialApp(
-      home: Scaffold(
-        body: Container(
-          decoration: BoxDecoration(
-            gradient: LinearGradient(colors: [
-              const Color.fromARGB(255, 75, 4, 91),
-              const Color.fromARGB(255, 91, 4, 81)
-            ],
-            begin: Alignment.topLeft,
-            end: Alignment.bottomRight
-            )
-          ),
-          child: activeScreen),
-      ),
-    );
+  Widget build(context) {
+    return MaterialApp(home: Scaffold(body: Container(child: activeScreen)));
   }
 }
--- lib/start_screen.dart
+++ lib/start_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class StartScreen extends StatelessWidget {
-  const StartScreen(this.StartQuiz,{super.key});
+  const StartScreen(this.StartQuiz, {super.key});
   final void Function() StartQuiz;
 
   @override
@@ -10,26 +10,9 @@ class StartScreen extends StatelessWidget {
       child: Column(
         mainAxisSize: MainAxisSize.min,
         children: [
-          Image.asset(
-            'assets/images/quiz-logo.png',
-            width: 300,
-            color: const Color.fromARGB(115, 251, 250, 249),
-          ),
-          const SizedBox(height: 50),
-          const Text(
-            "Learn Flutter Fun Way!",
-            style: TextStyle(
-              color: Colors.white,
-              fontSize: 25,
-            ),
-          ),
-          const SizedBox(height: 50),
+          const Text("Learn Flutter Fun Way!"),
           ElevatedButton.icon(
             onPressed: StartQuiz,
-            style: ElevatedButton.styleFrom(
-              foregroundColor: const Color.fromARGB(255, 91, 4, 81),
-            ),
-            icon: const Icon(Icons.arrow_right_alt),
             label: const Text("Start Quiz"),
           ),
         ],
